f88377048ef0a20d4a043bfa6ebaeca78dc4f564
Pilot1782 <34791564a@gmail.com>
2023-01-13 10:58:14 -0700

    support for onefile compiling

--- amogusus.py
+++ amogusus.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import ctypes
 import rotatescreen
 from playsound import playsound
@@ -8,9 +9,19 @@ from time import sleep
 from threading import Thread
 
 if __name__ == '__main__':
+    def resource_path(relative_path):
+        """ Get absolute path to resource, works for dev and for PyInstaller """
+        try:
+            # PyInstaller creates a temp folder and stores path in _MEIPASS
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+
+        return os.path.join(base_path, relative_path)
+
     def is_64bit_windows():
         try:
-            return os.uname()[-1]
+            return os.name == 'nt' and os.environ['PROCESSOR_ARCHITECTURE'].endswith('64')
         except AttributeError:
             if 'PROCESSOR_ARCHITEW6432' in os.environ:
                 return False
@@ -25,20 +36,20 @@ if __name__ == '__main__':
     
     def change_cur(cur):
         commands = [
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v Arrow /t REG_EXPAND_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v Crosshair /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v Hand /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v Help /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v IBeam /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v No /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v NWPen /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v SizeAll /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v SizeNESW /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v SizeNS /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v SizeNWSE /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v SizeWE /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v UpArrow /t REG_SZ /d "{cur}" /f',
-            f'REG ADD "HKEY_CURRENT_USER\Control Panel\Cursors" /v Wait /t REG_SZ /d "{cur}" /f'
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v Arrow /t REG_EXPAND_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v Crosshair /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v Hand /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v Help /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v IBeam /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v No /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v NWPen /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v SizeAll /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v SizeNESW /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v SizeNS /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v SizeNWSE /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v SizeWE /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v UpArrow /t REG_SZ /d "{cur}" /f',
+            f'REG ADD "HKEY_CURRENT_USER\\Control Panel\\Cursors" /v Wait /t REG_SZ /d "{cur}" /f'
         ]
 
         for i in range(0, len(commands)):
@@ -59,17 +70,17 @@ if __name__ == '__main__':
     print('Amogus.exe executed......')
 
     for i in range(1, 51):
-        make_shortcut('https://store.steampowered.com/app/945360/Among_Us/', name='Amogus_' + str(i), icon='amogus.ico', desktop=True)
+        make_shortcut('https://store.steampowered.com/app/945360/Among_Us/', name='Amogus_' + str(i), icon=resource_path('amogus.ico'), desktop=True)
     print('Shortcuts created!!111')
 
-    change_bg(os.path.abspath('wallpaper.jpg'))
+    change_bg(os.path.abspath(resource_path('wallpaper.jpg')))
     print('Wallpaper changed!1!11')
 
-    change_cur(os.path.abspath(r'cursor.cur'))
+    change_cur(os.path.abspath(resource_path('cursor.cur')))
     ctypes.windll.user32.SystemParametersInfoA(0x57)
     print('Cursor changed!!1!1111')
 
-    sound_path = os.path.abspath('AMOGUS.mp3')
+    sound_path = os.path.abspath(resource_path('AMOGUS.mp3')).replace('"', "'")
     Thread(target = playsound, args=(sound_path,)).start()
     Thread(target = fun_with_screen).start()
     print('Music playing!111!')
